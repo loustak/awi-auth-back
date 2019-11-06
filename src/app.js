2bce97f521d826e6030d539dd48c9b6f1e3b0c0c
Benchraa <soufianebenchraa2@gmail.com>
2019-11-06 23:12:53 +0100

    passing style

--- src/app.js
+++ src/app.js
@@ -5,22 +5,22 @@ app.get('/', (req, res) => {
   res.status(200).send('Homepage of the AWI auth api.')
 })
 app.post('/oAuth2/token', (req, res) => {
-  //post the token
+  //  post the token
 })
 
-app.post('/oAuth2/authorization', (req, res) => {
-// post the authorization code
+app.post('/oauth2/authorization', (req, res) => {
+  // post the authorization code
 })
-app.get('/oAuth2/authorize', (req, res) => {
-  //get client authorization request
-})
-
-app.get('/oAuth2/tokenRequest', (req, res) => {
-  //get token request
+app.get('/oauth2/authorize', (req, res) => {
+  //  get client authorization request
 })
 
-app.get('/oAuth2/authentication', (req, res) => {
-  //get authentication request
+app.get('/oauth2/tokenRequest', (req, res) => {
+  //  get token request
+})
+
+app.get('/oauth2/authentication', (req, res) => {
+  //  get authentication request
 })
 
 app.use((req, res, next) => {
